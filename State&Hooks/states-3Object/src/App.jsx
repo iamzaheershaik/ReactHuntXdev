b477c3fc8dcaf9b57a3010a077e5dc6aa3984f5f
itsmohdzaheer <zaheershaik0323@gmail.com>
2026-01-26 19:17:13 +0530

    State-3Object

---
+++ State&Hooks/states-3Object/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import React from 'react'
+
+const App = () => {
+  const [movie, setMovie] = useState({
+    title:"chichore",
+    ratings:"8.5"
+  });
+
+  const handleRating = () => {
+    setMovie({...movie, ratings:5})
+  }
+  return (
+    <section>
+      <h1>{movie.title}</h1>
+      <p>{movie.ratings}</p>
+      <button onClick={handleRating}>changeRating</button>
+    </section>
+  )
+}
+
+export default App
