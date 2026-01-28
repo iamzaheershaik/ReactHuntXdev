9b427e752056b9c0825084e4aa212df08d6ee7d6
itsmohdzaheer <zaheershaik0323@gmail.com>
2026-01-28 11:01:13 +0530

    Portal-1

---
+++ Portals/Portal-1/src/App.jsx
@@ -0,0 +1,13 @@
+import Copy from "./components/Copy"
+import PopupContent from "./components/PopupContent"
+
+const App = () => {
+
+  return (
+    <div>
+      <Copy />
+      </div>
+  )
+}
+
+export default App
