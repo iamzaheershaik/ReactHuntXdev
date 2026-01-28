2c53a72b7f846c1bf4cd5f111a380cb069506866
iamzaheershaik <zaheershaik0323@gmail.com>
2026-01-28 14:31:01 +0530

    SubmitWorkPortal

---
+++ Portals/Portal-2/src/components/Switcher.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+
+const Switcher = () => {
+    const [sw, setSw] = useState(false)
+  return (
+    <div>
+      {
+        sw ? (<span>Dark</span>) : (<span>Light</span>)
+      }
+      <br />
+      <input type="text" key={sw ? "dark":"light"} placeholder="enter you name"/>
+      <button onClick={() => setSw((s) => !s)}>Switch</button>
+    </div>
+  )
+}
+
+export default Switcher
